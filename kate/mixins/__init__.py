79a5539ed19a07f7ddfcee9dd50f856c4bdf6e8e
Polina Migel <polinamigel@gmail.com>
2025-07-30 11:40:24 +0300

    Move logic related to text attributes to `TextAttributesMixin`

--- kate/mixins/__init__.py
+++ kate/mixins/__init__.py
@@ -2,8 +2,10 @@
 
 from kate.mixins.bit_flags import BitFlagsMixin
 from kate.mixins.screen_buffer import ScreenBufferMixin
+from kate.mixins.text_attributes import TextAttributesMixin
 
 __all__ = (
     'BitFlagsMixin',
     'ScreenBufferMixin',
+    'TextAttributesMixin',
 )
