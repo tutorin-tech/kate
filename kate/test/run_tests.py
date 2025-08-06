061ddc8e055850ccb0fb7444b76ecaf02fd95b69
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:05:35 +0300

    tests: implement `AbsolutePositioningTest`

---
+++ kate/test/run_tests.py
@@ -0,0 +1,10 @@
+"""The module runs all the tests."""
+
+# ruff: noqa: F401
+
+import unittest
+
+from kate.test.test_mixins.test_absolute_positioning import AbsolutePositioningTest
+
+if __name__ == '__main__':
+    unittest.main()
