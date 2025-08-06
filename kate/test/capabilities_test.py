20bc82673b153cf52233392fe1a2f98144a55aed
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:27:18 +0300

    tests: implement `CharacterOperationsTest`

--- kate/test/capabilities_test.py
+++ kate/test/capabilities_test.py
@@ -323,30 +323,6 @@ class TestCapabilities(Helper):  # noqa: PLR0904
         self._terminal._cap_op()
         self.assertEqual(BLACK_AND_WHITE, self._terminal._sgr)
 
-    def test_cap_rc(self):
-        """The terminal should have the possibility to restore the cursor to
-        the last saved position.
-        """
-        term = self._terminal
-        term._cur_x = term._right_most - 1
-
-        # Put a character to move the cursor to the right-most position.
-        term._echo('e')
-
-        # After putting another character we will reach the end of the line.
-        term._echo('g')
-        self.assertTrue(term._eol)
-
-        cur_x_bck = term._cur_x
-        term._cap_sc()  # save the cursor current position.
-
-        # Put one more character to move the cursor to the next line.
-        term._echo('g')
-
-        term._cap_rc()  # restore a previously saved cursor position.
-        self.assertEqual(cur_x_bck, term._cur_x)
-        self.assertTrue(term._eol)
-
     def test_cap_rev(self):
         """The terminal should have the possibility to enable Reverse Video mode."""
         term = self._terminal
@@ -378,19 +354,6 @@ class TestCapabilities(Helper):  # noqa: PLR0904
         self.assertEqual(0, self._terminal._cur_x)
         self.assertEqual(0, self._terminal._cur_y)
         self.assertFalse(self._terminal._eol)
-
-    def test_cap_sc(self):
-        """The terminal should have the possibility to save the current cursor
-        position.
-        """
-        term = self._terminal
-        x = random.randint(0, term._right_most)
-        y = random.randint(0, term._bottom_most)
-        term._cur_x, term._cur_y = x, y
-        term._cap_sc()
-
-        self.assertEqual(x, term._cur_x_bak)
-        self.assertEqual(y, term._cur_y_bak)
 
     def test_cap_sgr0(self):
         """The terminal should have the possibility to turn off all attributes."""
--- kate/test/run_tests.py
+++ kate/test/run_tests.py
@@ -8,6 +8,7 @@ from kate.test.test_mixins.test_absolute_positioning import AbsolutePositioningT
 from kate.test.test_mixins.test_arrow_key_handling import ArrowKeyHandlingMixinTest
 from kate.test.test_mixins.test_basic_cursor_movements import BasicCursorMovementTest
 from kate.test.test_mixins.test_character_operations import CharacterOperationsTest
+from kate.test.test_mixins.test_cursor_state import CursorStateTest
 
 if __name__ == '__main__':
     unittest.main()
