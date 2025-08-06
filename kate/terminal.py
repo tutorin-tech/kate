ff8af2b36c5c791e7b87686d1397f4304d7e78c5
Polina Migel <polinamigel@gmail.com>
2025-08-06 18:32:52 +0300

    backend: move logic related to clearing screen to `ScreenClearingMixin`

--- kate/terminal.py
+++ kate/terminal.py
@@ -251,25 +251,6 @@ class Terminal:
         self._cap_vpa(y)
         self._cap_hpa(x)
 
-    def _cap_ed(self):
-        """Clear the screen from the current cursor position to the end of the
-        screen.
-        """
-        self._zero((self._cur_x, self._cur_y), (self._cols, self._rows - 1))
-
-    def _cap_el(self):
-        """Clear a line from the current cursor position to the end of the
-        line without moving the cursor. See _cap_el1.
-        """
-        self._zero((self._cur_x, self._cur_y), (self._cols, self._cur_y))
-
-    def _cap_el1(self):
-        """Clear a line from the beginning to the current cursor position,
-        inclusive. The cursor is not moved. See _cap_el.
-        """
-        self._zero((0, self._cur_y), (self._cur_x, self._cur_y),
-                   inclusively=True)
-
     def _cap_home(self):
         """Move the cursor to the home position."""
         self._cur_x = 0
