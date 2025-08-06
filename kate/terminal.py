5e32c73d1cc7470ac6a989d95dbf74b3819e6a17
Polina Migel <polinamigel@gmail.com>
2025-08-06 18:49:22 +0300

    backend: move logic related to pressing arrow keys to `ArrowKeyHandlingMixin`

--- kate/terminal.py
+++ kate/terminal.py
@@ -202,30 +202,6 @@ class Terminal:
         """Scroll the screen up moving its content down."""
         self._cursor_down()
 
-    def _cap_kb2(self):
-        """Handle a Center key-press on keypad."""
-        # xterm and Linux console have the kb2 capability, but screen doesn't.
-        # Some terminal emulators even handle it in spite of the seeming
-        # uselessness of the capability.
-        # It's been decided to have a do-nothing handler for kb2.
-
-    def _cap_kcub1(self):
-        """Handle a Left Arrow key-press."""
-        self._cur_x = max(0, self._cur_x - 1)
-        self._eol = False
-
-    def _cap_kcud1(self):
-        """Handle a Down Arrow key-press."""
-        self._cap_cud(1)
-
-    def _cap_kcuf1(self):
-        """Handle a Right Arrow key-press."""
-        self._cap_cuf(1)
-
-    def _cap_kcuu1(self):
-        """Handle an Up Arrow key-press."""
-        self._cur_y = max(self._top_most, self._cur_y - 1)
-
     def _cap_ri(self):
         """Scroll text down. See _cap_ind."""
         self._cur_y = max(self._top_most, self._cur_y - 1)
