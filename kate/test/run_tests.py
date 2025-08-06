9cd5e4b954c84fe9032faaa07c7eb994349adf20
Polina Migel <polinamigel@gmail.com>
2025-08-06 22:11:57 +0300

    tests: implement `ArrowKeyHandlingMixinTest`

--- kate/test/run_tests.py
+++ kate/test/run_tests.py
@@ -5,6 +5,7 @@
 import unittest
 
 from kate.test.test_mixins.test_absolute_positioning import AbsolutePositioningTest
+from kate.test.test_mixins.test_arrow_key_handling import ArrowKeyHandlingMixinTest
 
 if __name__ == '__main__':
     unittest.main()
