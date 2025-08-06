7b4b6862eb614fa38aaa4e2adb3480081e2bf3fb
Polina Migel <polinamigel@gmail.com>
2025-08-06 22:16:49 +0300

    tests: rename `TestCapabilities` to `TerminalTest`

--- kate/test/run_tests.py
+++ kate/test/run_tests.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+from kate.test.terminal_test import TerminalTest
 from kate.test.test_mixins.test_absolute_positioning import AbsolutePositioningTest
 from kate.test.test_mixins.test_arrow_key_handling import ArrowKeyHandlingMixinTest
 from kate.test.test_mixins.test_basic_cursor_movements import BasicCursorMovementTest
