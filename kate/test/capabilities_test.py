aba905df7b773848d5807e3ae721920e53183a61
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:56:16 +0300

    tests: implement `TextAttributesTest`

--- kate/test/capabilities_test.py
+++ kate/test/capabilities_test.py
@@ -20,13 +20,6 @@
 import random
 import unittest
 
-from kate.constants import (
-    BLACK_AND_WHITE,
-    BLINK_BIT,
-    BOLD_BIT,
-    REVERSE_BIT,
-    UNDERLINE_BIT,
-)
 from kate.test.helper import Helper
 
 
@@ -71,32 +64,6 @@ class TestCapabilities(Helper):
         self.assertEqual(1, term._cur_y)
         self.assertFalse(term._eol)
 
-    def test_cap_blink(self):
-        """The terminal should have the possibility to produce blinking text."""
-        term = self._terminal
-        term._cap_blink()
-        self.assertTrue(term._is_bit_set(BLINK_BIT, term._sgr))
-
-    def test_cap_bold(self):
-        """The terminal should have the possibility to produce bold text."""
-        term = self._terminal
-        term._cap_bold()
-        self.assertTrue(term._is_bit_set(BOLD_BIT, term._sgr))
-
-    def test_cap_op(self):
-        """The terminal should have the possibility to set default color-pair
-        to the original one.
-        """
-        self._terminal._sgr = None
-        self._terminal._cap_op()
-        self.assertEqual(BLACK_AND_WHITE, self._terminal._sgr)
-
-    def test_cap_rev(self):
-        """The terminal should have the possibility to enable Reverse Video mode."""
-        term = self._terminal
-        term._cap_rev()
-        self.assertTrue(term._is_bit_set(REVERSE_BIT, term._sgr))
-
     def test_cap_rs1(self):
         """The terminal should have the possibility to completely reset to sane
         modes.
@@ -112,25 +79,6 @@ class TestCapabilities(Helper):
         self.assertEqual(0, self._terminal._cur_x)
         self.assertEqual(0, self._terminal._cur_y)
         self.assertFalse(self._terminal._eol)
-
-    def test_cap_sgr0(self):
-        """The terminal should have the possibility to turn off all attributes."""
-        self._terminal._sgr = None
-        self._terminal._cap_sgr0()
-        self.assertEqual(BLACK_AND_WHITE, self._terminal._sgr)
-
-    def test_cap_smso(self):
-        """The terminal should have the possibility to enter Standout mode."""
-
-    def test_cap_smul_rmul(self):
-        """The terminal should have the possibility to enter and exit
-        Underline mode.
-        """
-        term = self._terminal
-        term._cap_smul()
-        self.assertTrue(term._is_bit_set(UNDERLINE_BIT, term._sgr))
-        term._cap_rmul()
-        self.assertFalse(term._is_bit_set(UNDERLINE_BIT, term._sgr))
 
 
 if __name__ == '__main__':
