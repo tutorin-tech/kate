84f7fc7f88e21fb4d805b5c84047f867a96c486a
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:43:29 +0300

    tests: implement `ScreenBufferTest`

--- kate/test/run_tests.py
+++ kate/test/run_tests.py
@@ -10,6 +10,7 @@ from kate.test.test_mixins.test_basic_cursor_movements import BasicCursorMovemen
 from kate.test.test_mixins.test_character_operations import CharacterOperationsTest
 from kate.test.test_mixins.test_cursor_state import CursorStateTest
 from kate.test.test_mixins.test_line_operations import LineOperationsTest
+from kate.test.test_mixins.test_screen_buffer import ScreenBufferTest
 
 if __name__ == '__main__':
     unittest.main()
