06f9af8c7bc501253102eed9a7488a47f21a6f85
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:49:45 +0300

    tests: implement `ScrollingControlTest`

--- kate/test/capabilities_test.py
+++ kate/test/capabilities_test.py
@@ -83,27 +83,6 @@ class TestCapabilities(Helper):
         term._cap_bold()
         self.assertTrue(term._is_bit_set(BOLD_BIT, term._sgr))
 
-    def test_cap_csr(self):
-        """The terminal should have the possibility to change the scrolling
-        region.
-        """
-        self._check_cap_csr((1, 1))
-        self._check_cap_csr((1, 2))
-        self._check_cap_csr((2, 1))
-
-        rand_top = random.randint(2, self._rows - 1)
-        rand_bottom = random.randint(2, self._rows - 1)
-        self._check_cap_csr((rand_top, rand_bottom))
-
-        self._check_cap_csr((self._rows, self._rows))
-
-    def test_cap_ind(self):
-        """The terminal should have the possibility to move the cursor down by
-        1 position.
-        """
-        self._terminal._cap_ind()
-        self.assertEqual(1, self._terminal._cur_y)
-
     def test_cap_op(self):
         """The terminal should have the possibility to set default color-pair
         to the original one.
@@ -117,16 +96,6 @@ class TestCapabilities(Helper):
         term = self._terminal
         term._cap_rev()
         self.assertTrue(term._is_bit_set(REVERSE_BIT, term._sgr))
-
-    def test_cap_ri(self):
-        """The terminal should have the possibility to scroll text down."""
-        term = self._terminal
-
-        self._check_cap_ri(['x'] * term._right_most, (0, 0))
-        self._check_cap_ri(['x'] * term._right_most, (0, 1))
-
-        rand_y = random.randint(2, term._bottom_most)
-        self._check_cap_ri(['x'] * term._right_most, (0, rand_y))
 
     def test_cap_rs1(self):
         """The terminal should have the possibility to completely reset to sane
