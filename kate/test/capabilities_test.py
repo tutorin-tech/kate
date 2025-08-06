c8ba4433f20a36ed5379418ba12f93d5fb12a9a3
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:31:19 +0300

    tests: implement `LineOperationsTest`

--- kate/test/capabilities_test.py
+++ kate/test/capabilities_test.py
@@ -203,48 +203,6 @@ class TestCapabilities(Helper):  # noqa: PLR0904
 
         self._check_cap_csr((self._rows, self._rows))
 
-    def test_cap_dl(self):
-        """The terminal should have the possibility to delete ``n`` number of
-        lines.
-        """
-        term = self._terminal
-
-        self._check_cap_dl(0, [(['f'] * term._cols, (0, 0))])
-        self._check_cap_dl(1, [(['f'] * term._cols, (0, 0))])
-
-        self._check_cap_dl(1, [
-            (['t'] * term._cols, (0, 0)),
-            (['a'] * term._cols, (0, 1)),
-        ])
-
-        self._check_cap_dl(2, [
-            (['t'] * term._cols, (0, 0)),
-            (['k'] * term._cols, (0, 1)),
-        ])
-
-        self._check_cap_dl(2, [
-            (['f'] * term._cols, (0, 0)),
-            (['s'] * term._cols, (0, 1)),
-            (['k'] * term._cols, (0, 2)),
-        ])
-
-        self._check_cap_dl(1, [
-            (['f'] * term._cols, (0, 0)),
-            (['s'] * term._cols, (0, 1)),
-            (['k'] * term._cols, (0, 2)),
-        ])
-
-        self._check_cap_dl(0, [
-            (['f'] * term._cols, (0, 0)),
-            (['s'] * term._cols, (0, 1)),
-            (['k'] * term._cols, (0, 2)),
-        ])
-
-        lines_number = random.randint(2, term._bottom_most)
-        lines = [(['a'] * term._cols, (0, i)) for i in range(lines_number)]
-
-        self._check_cap_dl(random.randint(0, lines_number), lines)
-
     def test_cap_ed(self):
         """The terminal should have the possibility to clear the screen from
         the current cursor position to the end of the screen.
@@ -297,16 +255,6 @@ class TestCapabilities(Helper):  # noqa: PLR0904
 
         rand_x = random.randint(1, term._right_most - 1)
         self._check_cap_el1(['s'] * term._right_most, (rand_x, 0))
-
-    def test_cap_il1(self):
-        """The terminal should have the possibility to add a new blank line."""
-        term = self._terminal
-
-        self._check_cap_il1(['s'] * term._right_most, (0, 0))
-        self._check_cap_il1(['s'] * term._right_most, (0, term._bottom_most))
-
-        rand_y = random.randint(1, term._bottom_most - 1)
-        self._check_cap_il1(['s'] * term._right_most, (0, rand_y))
 
     def test_cap_ind(self):
         """The terminal should have the possibility to move the cursor down by
--- kate/test/run_tests.py
+++ kate/test/run_tests.py
@@ -9,6 +9,7 @@ from kate.test.test_mixins.test_arrow_key_handling import ArrowKeyHandlingMixinT
 from kate.test.test_mixins.test_basic_cursor_movements import BasicCursorMovementTest
 from kate.test.test_mixins.test_character_operations import CharacterOperationsTest
 from kate.test.test_mixins.test_cursor_state import CursorStateTest
+from kate.test.test_mixins.test_line_operations import LineOperationsTest
 
 if __name__ == '__main__':
     unittest.main()
