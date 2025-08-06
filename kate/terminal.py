3af2584b70d0a80937cac0f378ca1fc042d7bf76
Polina Migel <polinamigel@gmail.com>
2025-08-06 20:39:55 +0300

    backend: move logic related to scrolling control to `ScrollingControlMixin`

--- kate/terminal.py
+++ kate/terminal.py
@@ -120,31 +120,6 @@ class Terminal:
         self._screen[pos] = self._sgr | ord(c)
         self._cursor_right()
 
-    def _cap_csr(self, top, bottom):
-        """Change the scrolling region.
-
-        The ``top`` and ``bottom`` parameters are lines of the scrolling
-        region. After executing the method, the cursor position is undefined.
-        See _cap_sc and _cap_rc.
-
-        The ``top`` and ``bottom`` values start from 1.
-        """
-        self._top_most = min(self._bottom_most, top - 1)
-        self._bottom_most = min(self._bottom_most, bottom - 1)
-
-        # `_bottom_most` must be greater than or equal to `_top_most`.
-        self._bottom_most = max(self._top_most, self._bottom_most)
-
-    def _cap_ind(self):
-        """Scroll the screen up moving its content down."""
-        self._cursor_down()
-
-    def _cap_ri(self):
-        """Scroll text down. See _cap_ind."""
-        self._cur_y = max(self._top_most, self._cur_y - 1)
-        if self._cur_y == self._top_most:
-            self._scroll_down(self._top_most, self._bottom_most)
-
     def _cap_rs1(self):
         """Reset terminal completely to sane modes."""
         cells_number = self._cols * self._rows
