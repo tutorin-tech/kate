aaf25337b32b4944ac1c1e88ae7089e7967798b4
Polina Migel <polinamigel@gmail.com>
2025-08-06 21:14:57 +0300

    tests: implement `BasicCursorMovementTest`

--- kate/test/run_tests.py
+++ kate/test/run_tests.py
@@ -6,6 +6,7 @@ import unittest
 
 from kate.test.test_mixins.test_absolute_positioning import AbsolutePositioningTest
 from kate.test.test_mixins.test_arrow_key_handling import ArrowKeyHandlingMixinTest
+from kate.test.test_mixins.test_basic_cursor_movements import BasicCursorMovementTest
 
 if __name__ == '__main__':
     unittest.main()
