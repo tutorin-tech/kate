ef29270c2e50d38cbf70298cfd391efb994f5b41
Polina Migel <polinamigel@gmail.com>
2025-10-28 15:24:17 +0300

    Move logic related to internal screen buffer to `ScreenBufferMixin`

--- kate/mixins/__init__.py
+++ kate/mixins/__init__.py
@@ -3,9 +3,11 @@
 from kate.mixins.content import ContentMixin
 from kate.mixins.core import CoreMixin
 from kate.mixins.cursor import CursorMixin
+from kate.mixins.screen_buffer import ScreenBufferMixin
 
 __all__ = (
     'ContentMixin',
     'CoreMixin',
     'CursorMixin',
+    'ScreenBufferMixin',
 )
--- kate/terminal.py
+++ kate/terminal.py
@@ -17,7 +17,6 @@
 
 # ruff: noqa: PLR2004
 
-import array
 import html
 import json
 import logging
@@ -39,6 +38,7 @@ class Terminal(
     mixins.ContentMixin,
     mixins.CoreMixin,
     mixins.CursorMixin,
+    mixins.ScreenBufferMixin,
 ):
     """The class implements a terminal."""
 
@@ -112,71 +112,6 @@ class Terminal(
     #
     # Internal methods.
     #
-    def _peek(self, left_border, right_border, *, inclusively=False):
-        """Capture and returns a rectangular region of the screen between
-        ``left_border`` and ``right_border``.
-
-        The ``left_border`` and ``right_border`` arguments must be tuples or
-        lists of coordinates ``(x1, y1)`` and ``(x2, y2)``, respectively.
-
-        The name of the method was inherited from AjaxTerm, developers of
-        which, in turn, inherited it from BASIC. See _poke.
-        """
-        x1, y1 = left_border
-        x2, y2 = right_border
-        begin = self._cols * y1 + x1
-        end = self._cols * y2 + x2 + (1 if inclusively else 0)
-        return self._screen[begin:end]
-
-    def _poke(self, pos, s):
-        """Put the specified slice ``s`` on the screen staring at the position
-        ``pos``.
-
-        The ``pos`` argument must be a tuple or list of coordinates ``(x, y)``.
-
-        The name of the method was inherited from AjaxTerm, developers of
-        which, in turn, inherited it from BASIC. See _peek.
-        """
-        x, y = pos
-        begin = self._cols * y + x
-        self._screen[begin:begin + len(s)] = s
-
-    def _zero(self, left_border, right_border, *, inclusively=False):
-        """Clear the area from ``left_border`` to ``right_border``.
-
-        The ``left_border`` and ``right_border`` arguments must be tuples or
-        lists of coordinates ``(x1, y1)`` and ``(x2, y2)``, respectively.
-        """
-        x1, y1 = left_border
-        x2, y2 = right_border
-        begin = self._cols * y1 + x1
-        end = self._cols * y2 + x2 + (1 if inclusively else 0)
-        length = end - begin  # the length of the area which have to be cleared
-        self._screen[begin:end] = array.array('Q', [BLACK_AND_WHITE] * length)
-        return length
-
-    def _scroll_up(self, y1, y2):
-        """Move the area specified by coordinates 0, ``y1`` and 0, ``y2`` up 1
-        row.
-        """
-        area = self._peek((0, y1), (self._right_most, y2), inclusively=True)
-        self._poke((0, y1 - 1), area)  # move the area up 1 row (y1 - 1)
-        self._zero((0, y2), (self._cols, y2))
-
-    def _scroll_down(self, y1, y2):
-        """Move the area specified by coordinates 0, ``y1`` and 0, ``y2`` down
-        1 row.
-        """
-        line = self._peek((0, y1), (self._cols, y2 - 1))
-        self._poke((0, y1 + 1), line)
-        self._zero((0, y1), (self._cols, y1))
-
-    def _scroll_right(self, x, y):
-        """Move a piece of a row specified by coordinates ``x`` and ``y``
-        right by 1 position.
-        """
-        self._poke((x + 1, y), self._peek((x, y), (self._cols, y)))
-        self._zero((x, y), (x, y), inclusively=True)
 
     def _echo(self, c):
         """Put the specified character ``c`` on the screen and moves the
