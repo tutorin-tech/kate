4a008eb5ca3b5bcd0447f7606aaada12eef81131
Polina Migel <polinamigel@gmail.com>
2025-10-28 15:54:28 +0300

    Move logic related to visual attributes to `VisualAttributesMixin`

--- kate/mixins/__init__.py
+++ kate/mixins/__init__.py
@@ -4,10 +4,12 @@ from kate.mixins.content import ContentMixin
 from kate.mixins.core import CoreMixin
 from kate.mixins.cursor import CursorMixin
 from kate.mixins.screen_buffer import ScreenBufferMixin
+from kate.mixins.visual_attributes import VisualAttributesMixin
 
 __all__ = (
     'ContentMixin',
     'CoreMixin',
     'CursorMixin',
     'ScreenBufferMixin',
+    'VisualAttributesMixin',
 )
