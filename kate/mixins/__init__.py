99ed131d22b76f26b86c37a391b395e108e96898
Polina Migel <polinamigel@gmail.com>
2025-10-28 15:03:51 +0300

    Move logic related to core infrastructure to `CoreMixin`

--- kate/mixins/__init__.py
+++ kate/mixins/__init__.py
@@ -1,7 +1,9 @@
 """The package contains mixins for extending the terminal functionality."""
 
 from kate.mixins.content import ContentMixin
+from kate.mixins.core import CoreMixin
 
 __all__ = (
     'ContentMixin',
+    'CoreMixin',
 )
