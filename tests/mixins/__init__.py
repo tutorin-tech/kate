12ac80fc683cf8bb4721061e177a795571570395
Polina Migel <polinamigel@gmail.com>
2025-10-29 13:01:53 +0300

    tests: implement `TestScreenBufferMixin`

--- tests/mixins/__init__.py
+++ tests/mixins/__init__.py
@@ -2,8 +2,10 @@
 
 from tests.mixins.test_content_mixin import TestContentMixin
 from tests.mixins.test_cursor_mixin import TestCursorMixin
+from tests.mixins.test_screen_buffer_mixin import TestScreenBufferMixin
 
 __all__ = (
     'TestContentMixin',
     'TestCursorMixin',
+    'TestScreenBufferMixin',
 )
